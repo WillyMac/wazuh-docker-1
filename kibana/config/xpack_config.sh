ed7ec11f3cc6443e56f6121b5536ce7ce754e8ec
Jesus Linares <jesuslinares90@gmail.com>
2019-09-26 15:18:23 +0200

    Remove ciphers, default

--- kibana/config/xpack_config.sh
+++ kibana/config/xpack_config.sh
@@ -37,7 +37,6 @@ xpack.maps.enabled: $XPACK_MAPS
 xpack.uptime.enabled: $XPACK_UPTIME
 console.enabled: $XPACK_DEVTOOLS
 xpack.security.sessionTimeout: 900000
-server.ssl.cipherSuites: [\"HIGH\", \"!LOW\", \"!MEDIUM\", \"!SSLv2\", \"!ADH\", \"!EXP\", \"!MD5\", \"!RC4\", \"!3DES\", \"!CAMELLIA\", \"!aNULL\", \"!eNULL\", \"!DES\", \"!PSK\", \"!SRP\", \"!TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384\", \"!TLS_RSA_WITH_AES_256_GCM_SHA384\", \"!TLS_RSA_WITH_AES_256_CBC_SHA256\", \"@STRENGTH\"]
 
 " >> $kibana_config_file
 fi
